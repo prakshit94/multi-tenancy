5464c8ad307efa09a78bb2ddec2e3209b6369410
prakshit <shitalmakwana2206@gmail.com>
2026-03-11 21:58:41 +0530

    fix the UI issue

--- resources/views/central/orders/verification/index.blade.php
+++ resources/views/central/orders/verification/index.blade.php
@@ -57,7 +57,7 @@
         
         <!-- District Insights -->
         @if($districtCounts->count() > 0)
-            <div class="flex flex-col gap-2" x-data="{ showDistricts: true }">
+            <div class="flex flex-col gap-2" x-data="{ showDistricts: false }">
                 <div class="flex items-center justify-between px-1">
                     <div class="flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-primary">
